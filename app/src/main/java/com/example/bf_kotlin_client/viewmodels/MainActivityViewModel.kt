759fbc553b10038413fb4de638260cde26074e71
Innokentii <jacksafj@gmail.com>
2022-05-12 19:09:53 +0300

    comments

--- app/src/main/java/com/example/bf_kotlin_client/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/example/bf_kotlin_client/viewmodels/MainActivityViewModel.kt
@@ -8,9 +8,16 @@ import com.example.bf_kotlin_client.utils.AppFragmentManager
 import com.example.bf_kotlin_client.utils.AppFragmentManager.FragmentsName.*
 import com.example.bf_kotlin_client.utils.GlobalVariables
 
+/**
+ * Логика главного экрана приложения
+ *
+ */
 class MainActivityViewModel {
     private var fragmentManager = GlobalVariables.instance.fragmentManager
 
+    /**
+     * Словарь,соотносящий id кнопок BottomNavigationView с названиями вкладок [AppFragmentManager]
+     */
     private var menuItemIdToTabName: MutableMap<Int, AppFragmentManager.FragmentsName> =
         mutableMapOf(
             R.id.productsSection to ProductsCategoriesFragment,
@@ -20,6 +27,14 @@ class MainActivityViewModel {
             R.id.supportSection to SupportQuestionsPageFragment
         )
 
+    /**
+     * Обработчик события кнопки "назад"
+     *
+     * Привязывется через параметр [onBackPressed][com.example.bf_kotlin_client.utils.onBackPressed]
+     *
+     * Косвенно используется в параметре [is_back_button][com.example.bf_kotlin_client.utils.setIsBackButton]
+     *
+     */
     var onBackPressedCallback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
             fragmentManager.popBackStack()
@@ -30,6 +45,12 @@ class MainActivityViewModel {
         GlobalVariables.instance.onBackPressedCallback = onBackPressedCallback
     }
 
+    /**
+     * Вызывается при нажатии на кнопку BottomNavigationView
+     *
+     * @param menuItem элемент, на который нажали
+     * @return удалось ли нажать
+     */
     fun onItemSelected(menuItem: MenuItem): Boolean {
         var tabName = menuItemIdToTabName[menuItem.itemId]
         fragmentManager.showTab(tabName!!)
--- app/src/main/java/com/example/bf_kotlin_client/viewmodels/support/SupportQuestionsPageViewModel.kt
+++ app/src/main/java/com/example/bf_kotlin_client/viewmodels/support/SupportQuestionsPageViewModel.kt
@@ -4,21 +4,36 @@ import androidx.databinding.ObservableField
 import com.example.bf_kotlin_client.adapters.support.RvAdapterSupport
 import com.example.bf_kotlin_client.apiworkers.FaqsApiWorker
 import com.example.bf_kotlin_client.dtos.responses.FaqResponseDto
+import com.example.bf_kotlin_client.dtos.responses.FarmersResponseDto
 import com.google.gson.Gson
-
+/**
+ * Логика фрагмента [списка вопросов][com.example.bf_kotlin_client.fragments.support.SupportQuestionsPageFragment]
+ *
+ */
 class SupportQuestionsPageViewModel {
     private var faqApiWorker = FaqsApiWorker()
-
+    /**
+     * Поле для отображения списка фермеров в binding
+     */
     var rvAdapterSupport = ObservableField(RvAdapterSupport(arrayListOf()))
 
+    /**
+     * при инициализации делает запрос к серверу на получение всех ЧЗВ из базы данных
+     */
     init{
         update()
     }
-
+    /**
+     * при инициализации делает запрос к серверу на получение всех ЧЗВ из базы данных
+     */
     fun update(){
         faqApiWorker.getAll(::updateRv)
     }
-
+    /**
+     * Обновляет значение [rvAdapterSupport] используя переданные данные
+     *
+     * @param jsonData [FaqResponseDto] сущность в виде JSON строки
+     */
     fun updateRv(jsonData: String){
         var response = Gson().fromJson(jsonData, FaqResponseDto::class.java)
 
